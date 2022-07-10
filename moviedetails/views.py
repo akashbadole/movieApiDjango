f6e0e3087c040fad2def1eae8b3d1396604ae2b5
Akash B <akashbadole@rediffmail.com>
2022-07-10 23:13:39 +0530

    Database data

--- moviedetails/views.py
+++ moviedetails/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import loader
 from .models import Moviesdetail
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hi Welcome to Home Page")
-    templates = loader.get_template('home.html')
-    return HttpResponse(templates.render())
+    # templates = loader.get_template('home.html')
+    # return HttpResponse(templates.render())
+    mymovies  =  Moviesdetail.objects.all().values()
+    output    = ""
+    for m in mymovies:
+        output += m["Name"]
+        return HttpResponse(output)
+
